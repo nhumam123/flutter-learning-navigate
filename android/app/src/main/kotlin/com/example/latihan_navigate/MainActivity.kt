64e57ee44e7aae43b6129b7ee53a62580cef9aa6
nhumam123 <naufal.pujianputra@gmail.com>
2023-12-01 18:54:03 +0700

    Learning navigate flutter

---
+++ android/app/src/main/kotlin/com/example/latihan_navigate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_navigate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: FirstRoute());
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('halaman pertama'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Pergi ke halaman kedua'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Halaman kedua'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Kembali'),
+        ),
+      ),
+    );
+  }
+}
